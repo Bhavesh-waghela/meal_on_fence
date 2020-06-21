cefec2bbe865b04b92cbc65bb3c8fcfe5ce1d1ab
Bhavesh Waghela <bhaveshwaghela@Bhaveshs-MacBook-Air.local>
2020-06-21 02:00:57 +0200

    minor issue fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]        
         user.email = data["email"] if user.email.blank?
       end
     end
@@ -15,6 +15,7 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      user.username = auth.info.name
       user.password = Devise.friendly_token[0, 20]
     end
   end
